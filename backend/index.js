274005d6c9778e9cbdfe167ec4bdc6c8753ef19f
Anish <work.anish.004@gmail.com>
2024-08-23 18:52:07 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { mongoose } = require("mongoose");
 const mainRouter = require("./routes/user");
+const cors= require("cors")
 
 require("dotenv").config({ path: "./config/.env" });
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use("/api/v1", mainRouter);
