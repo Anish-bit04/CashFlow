94ba12b8991264c616108d75773ffa574fe42756
Anish <work.anish.004@gmail.com>
2024-08-24 08:34:07 +0530

    created sendMoney page and implemented all the api logic

--- frontend/README.md
+++ frontend/README.md
@@ -6,3 +6,11 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+# Things needed to add 
+- need to add debouncing and filter using lowercase in filter
+- optimize the code
+- add loader to Send Money and success alert or something 
+- close the routes so only logged in user can access the routes
+- make signin page functionable
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,26 +1,72 @@
-import React from 'react'
-import Heading from '../components/Heading'
-import SubHeading from '../components/SubHeading'
-import { BottomWarning } from '../components/BottomWarning'
-import InputBox from '../components/InputBox'
-import Button from '../components/Button'
+import React, { useEffect, useState } from "react";
+import Heading from "../components/Heading";
+import SubHeading from "../components/SubHeading";
+import { BottomWarning } from "../components/BottomWarning";
+import InputBox from "../components/InputBox";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 const Signup = () => {
-  return <div className="bg-slate-300 h-screen flex justify-center">
-  <div className="flex flex-col justify-center">
-    <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-      <Heading label={"Sign up"} />
-      <SubHeading label={"Enter your information to create an account"} />
-      <InputBox placeholder="Anish" label={"First Name"} />
-      <InputBox placeholder="Choudhary" label={"Last Name"} />
-      <InputBox placeholder="anishb2041@gmail.com" label={"Email"} />
-      <InputBox placeholder="123456" label={"Password"} />
-      <div className="pt-4">
-        <Button label={"Sign up"} />
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const SignupBtn = async () => {
+    const response = await axios.post(
+      "http://localhost:4000/api/v1/user/signup",
+      {
+        username,
+        firstName,
+        lastName,
+        password,
+      }
+    );
+    localStorage.setItem("token", response.data.token);
+    navigate("/dashboard");
+  };
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign up"} />
+          <SubHeading label={"Enter your information to create an account"} />
+          <InputBox
+            placeholder="Anish"
+            label={"First Name"}
+            onChange={(e) => setfirstName(e.target.value)}
+          />
+          <InputBox
+            placeholder="Choudhary"
+            label={"Last Name"}
+            onChange={(e) => setlastName(e.target.value)}
+          />
+          <InputBox
+            placeholder="anishb2041@gmail.com"
+            label={"Email"}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <InputBox
+            placeholder="123456"
+            label={"Password"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="pt-4">
+            <Button
+              label={"Sign up"}
+              onClick={SignupBtn}
+            />
+          </div>
+          <BottomWarning
+            label={"Already have an account?"}
+            buttonText={"Sign in"}
+            to={"/signin"}
+          />
+        </div>
       </div>
-      <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
     </div>
-  </div>
-</div>
-}
-export default Signup
+  );
+};
+export default Signup;
