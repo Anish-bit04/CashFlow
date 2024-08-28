9fa9673b52ef929175cd1c6b7648af5a42d8c90c
Anish <work.anish.004@gmail.com>
2024-08-28 20:02:42 +0530

    solved deployement error

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 function authmiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(403).json({});
   }
 
